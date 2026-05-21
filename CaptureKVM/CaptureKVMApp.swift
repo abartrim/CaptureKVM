74374d466c268521068b386c62db5b8da761c6e0
abartrim <abartrim@users.noreply.github.com>
2026-05-21 12:52:31 -0700

    Initial Commit

---
+++ CaptureKVM/CaptureKVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureKVMApp.swift
+//  CaptureKVM
+//
+//  Created by Aaron Bartrim on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct CaptureKVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
